a854c64c3bb5d0c00431cfb26cb08a7601b54000
Hermon Assefa <41701546+Hermonj@users.noreply.github.com>
2023-12-16 17:05:00 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,9 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import plotly.express as px
+import geopandas as gpd
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 #######################
 # Page configuration
@@ -40,3 +43,42 @@ highlight = pd.DataFrame({
  
 # Add the highlight points to the map
 st.map(data, highlight)
+
+
+
+
+
+
+# Load data (replace with your GDP data for each province)
+data = {
+    "Province": ["Beijing", "Shanghai", "Guangdong", "Zhejiang", "Jiangsu"],
+    "GDP": [36103, 39055, 145219, 62352, 102611]  # Example GDP values in billion USD
+}
+
+df = pd.DataFrame(data)
+
+# Load China map shapefile
+china_map = gpd.read_file("path/to/china_shapefile.shp")  # Replace with the path to your shapefile
+
+# Merge GDP data with China map
+merged_data = pd.merge(china_map, df, on="Province", how="left")
+
+# Streamlit app
+st.title("China GDP Ranking by Province")
+
+# Create a sidebar for user input
+selected_year = st.sidebar.slider("Select Year:", 2023, 2030, 2023, 1)
+
+# Plot the map
+fig, ax = plt.subplots(1, 1, figsize=(12, 10))
+divider = make_axes_locatable(ax)
+cax = divider.append_axes("right", size="5%", pad=0.1)
+
+merged_data.plot(column="GDP", cmap="OrRd", linewidth=0.8, ax=ax, edgecolor="0.8", legend=True, cax=cax)
+ax.set_title(f"China GDP by Province - {selected_year}")
+ax.set_axis_off()
+
+# Display the map using Streamlit
+st.pyplot(fig)
+
+
