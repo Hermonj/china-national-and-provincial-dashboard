12a874e74deadd4f51f3498d6189ffbf7c065d20
Hermon Assefa <41701546+Hermonj@users.noreply.github.com>
2023-12-16 14:46:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,15 +22,4 @@ alt.themes.enable("dark")
 
 #simply plotting a map with no parameters
 
-df = pd.DataFrame({
-    "col1": np.random.randn(1000) / 50 + 37.76,
-    "col2": np.random.randn(1000) / 50 + -122.4,
-    "col3": np.random.randn(1000) * 100,
-    "col4": np.random.rand(1000, 4).tolist(),
-})
-
-st.map(df,
-    latitude='col1',
-    longitude='col2',
-    size='col3',
-    color='col4')
+st.map(China)
