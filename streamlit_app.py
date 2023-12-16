a997a64c96a0c376b7fe3c7484837c47c7bc68c9
Hermon Assefa <41701546+Hermonj@users.noreply.github.com>
2023-12-16 14:43:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,12 +26,3 @@ st.map()
 
 
 
-#creating a sample data consisting different points 
-
-df = pd.DataFrame(np.random.randn(800, 2) / [50, 50] + [46.34, -108.7],columns=['latitude', 'longitude'])
-
-
-
-#plotting a map with the above defined points
-
-st.map(df)
