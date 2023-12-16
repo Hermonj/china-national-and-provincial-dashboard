073ab470890e22007c4068d9502fa5ff85f037aa
Hermon Assefa <41701546+Hermonj@users.noreply.github.com>
2023-12-16 14:54:25 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,4 +22,12 @@ alt.themes.enable("dark")
 
 #simply plotting a map with no parameters
 
-st.map(China)
+## Create a sample DataFrame with latitude and longitude values
+data = pd.DataFrame({
+    'latitude': [37.7749, 34.0522, 40.7128],
+    'longitude': [-122.4194, -118.2437, -74.0060]
+})
+ 
+## Create a map with the data
+st.map(data)
+
